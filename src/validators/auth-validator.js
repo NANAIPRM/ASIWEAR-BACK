b07a33c54e4213091db0c04aa95bc2ab13ee6bba
porramat <>
2023-07-11 11:28:15 +0700

    asd

--- src/validators/auth-validator.js
+++ src/validators/auth-validator.js
@@ -1,8 +1,7 @@
 const Joi = require("joi");
 const validate = require("./validate");
 const registerSchema = Joi.object({
-  firstName: Joi.string().trim().required(),
-  lastName: Joi.string().trim().required(),
+  Nickname: Joi.string().trim().required(),
   email: Joi.string().email({ tlds: false }).required(),
   password: Joi.string()
     .pattern(/^[a-zA-Z0-9]{6,30}$/)
